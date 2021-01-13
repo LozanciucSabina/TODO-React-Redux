c0d05ee16af6097334eebeb7394acd1b96ca4d53
Lozanciuc Sabina <sabinalozanciuc2000@gmail.com>
2021-01-13 16:36:54 +0200

    Firebase Authentication

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import "TodoComponent" from 
-function App() {
-  return <TodoComponent></TodoComponent>;
-}
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Components/Home";
+import Dashboard from "./Components/Dashboard";
+import LogIn from "./Components/LogIn";
+import SignUp from "./Components/SignUp";
+import { AuthProvider } from "./Components/Auth";
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/login" component={LogIn} />
+          <Route exact path="/signup" component={SignUp} />
+        </Switch>
+      </Router>
+    </AuthProvider>
+  );
+};
 
 export default App;
